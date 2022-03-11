0cc73d64a9e92109ac1d4e49dedf69c553a41351
Lorenzo Zulli <zullilorenzo.24@gmail.com>
2022-03-11 18:13:53 +0100

    aggiunta dipendenza jsonsimple, fatti altri lavori

--- progetto/src/main/java/com/univpm/progetto/Services/UpworkService.java
+++ progetto/src/main/java/com/univpm/progetto/Services/UpworkService.java
@@ -3,9 +3,12 @@ package com.univpm.progetto.Services;
 import com.univpm.progetto.StatsandFilters.Filters;
 import com.univpm.progetto.StatsandFilters.Stats;
 import com.univpm.progetto.Models.Freelancer;
+import com.univpm.progetto.Models.Users;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
+
+import java.util.Vector;
 
 /**
  * Classe contente i metodi di servizio
@@ -31,12 +34,12 @@ public class UpworkService {
      * @return stats
      */
     public JSONObject statsGenerator(){
-        Freelancer freelancer = new Freelancer();
+        Users users = 
         JSONObject stats = new JSONObject();
-        stats.put("average portfolio_items_count", this.stats.portfolioAverage());
-        stats.put("variance portfolio_items_count", this.stats.portfolioVariance());
-        stats.put("average skills", this.stats.skillAverage());
-        stats.put("variance skills", this.stats.skillVariance());
+        stats.put("average portfolio_items_count", this.stats.portfolioAverage(users.getFreelancer()));
+        stats.put("variance portfolio_items_count", this.stats.portfolioVariance(freelancer));
+        stats.put("average skills", this.stats.skillAverage(freelancer));
+        stats.put("variance skills", this.stats.skillVariance(freelancer));
         return stats;
     }
     /**
