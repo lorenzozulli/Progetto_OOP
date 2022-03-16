3bb52bacc0881b07b0d13c9d4a014e33ffe760e5
Giovanni Prati <94047126+jjohnnyUtah@users.noreply.github.com>
2022-03-16 23:16:11 +0100

    testati tutti i metodi di Stats

--- progetto/src/test/java/com/univpm/progetto/TestStatsandFilters/TestStats.java
+++ progetto/src/test/java/com/univpm/progetto/TestStatsandFilters/TestStats.java
@@ -18,15 +18,36 @@ class TestStats {
 		Freelancer freelancer0 = new Freelancer();
 		Freelancer freelancer1= new Freelancer();
 		Freelancer freelancer2 = new Freelancer();
+		
+		String[] skills0 = {"java",
+				"una_skill",
+				"una_skill",
+	            "una_skill",
+	            "una_skill",
+	            "una_skill"};
+		String[] skills1 = {"java",
+				"una_skill",
+				"una_skill",
+	            "una_skill",
+	            "una_skill",
+	            "una_skill"};
+		String[] skills2 = {"java",
+				"una_skill",
+				"una_skill"};
 
-		freelancer0.setPortfolio_items_count(5);
-		freelancer1.setPortfolio_items_count(30);
-		freelancer2.setPortfolio_items_count(25);
+		freelancer0.setSkills(skills0);
+		freelancer1.setSkills(skills1);
+		freelancer2.setSkills(skills2);
+
+		
 
 	}
 
 	@Test
-	void test() {
+	void test_portfolioAverage() {
+		freelancer0.setPortfolio_items_count(5);
+		freelancer1.setPortfolio_items_count(30);
+		freelancer2.setPortfolio_items_count(25);
 		users.add(freelancer0);
 		users.add(freelancer1);
 		users.add(freelancer2);
@@ -35,5 +56,41 @@ class TestStats {
 		assertEquals(20,output);
 		
 	}
+	@Test
+	void test_portfolioVariance() {
+		freelancer0.setPortfolio_items_count(5);
+		freelancer1.setPortfolio_items_count(15);
+		freelancer2.setPortfolio_items_count(25);
+		users.add(freelancer0);
+		users.add(freelancer1);
+		users.add(freelancer2);
 
+		double output = test.portfolioVariance(users);
+		assertEquals(100,output);
+		
+	}
+	
+	@Test
+	void test_skillAverage() {
+		
+		users.add(freelancer0);
+		users.add(freelancer1);
+		users.add(freelancer2);
+
+		double output = test.skillAverage(users);
+		assertEquals(5,output);
+		
+	}
+	
+	@Test
+	void test_skillVariance() {
+		
+		users.add(freelancer0);
+		users.add(freelancer1);
+		users.add(freelancer2);
+
+		double output = test.skillVariance(users);
+		assertEquals(3,output);
+		
+	}
 }
