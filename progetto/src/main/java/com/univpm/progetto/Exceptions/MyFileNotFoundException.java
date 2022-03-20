eee391be38fb59c1ac754b352d9f1c6a1716a904
giova <giova@LAPTOP-3ORNOM1Q>
2022-03-20 15:22:51 +0100

    exc

--- progetto/src/main/java/com/univpm/progetto/Exceptions/MyFileNotFoundException.java
+++ progetto/src/main/java/com/univpm/progetto/Exceptions/MyFileNotFoundException.java
@@ -1,7 +1,12 @@
 package com.univpm.progetto.Exceptions;
 
 public class MyFileNotFoundException extends Exception {
-    public MyFileNotFoundException() {
+   
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	public MyFileNotFoundException() {
         super();
     }
     public MyFileNotFoundException(String msg) {
